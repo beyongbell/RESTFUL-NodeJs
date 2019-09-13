5d80fddc40216575a37742a432f105be82cdc875
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-13 12:50:27 +0700

    Node Js MongoDv

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config()
+const app        = express();
+const mongoose   = require('mongoose');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Import Routers
 const postsRouter = require('./routes/posts')
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,56 @@
 const express = require('express');
 const router  = express.Router();
 const Post    = require('../models/Post');
-
-router.get('/', (req, res) => {
-    res.send("We are on posts")
+// Index
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts)
+    } catch(err) {
+        res.json({ message: err})
+    }
 })
+// SAVE
+router.post('/', async (req, res) => {
+    // post.save().then(data => { res.json(data) }).catch(err => { res.json({ message: err})})
+    const post = new Post({
+        title : req.body.title,
+        description: req.body.description
+    })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost);
+    } catch(err) {
+        res.json({message: err});
+    }
 })
+// SHOW
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post)
+  } catch (error) {
+    res.json({ message: error});
+  }
+})
+// UPDATE
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne({_id: req.params.postId},{ $set: {title : req.body.title, description : req.body.description} })
+        res.json(updatedPost)
+    } catch (error) {
+        res.json({message : error})
+    }
+})
+// DELETE POST
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removePost = await Post.remove({_id: req.params.postId})
+        res.json(removePost);
+    } catch (error) {
+        res.json({message: error});
+    }
+});
 
 module.exports = router;
